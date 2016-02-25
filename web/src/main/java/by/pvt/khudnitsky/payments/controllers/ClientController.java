bf525f22acca4ffdf329254040c1620edf30c4be
khudnitsky <khudnitsky@gmail.com>
2016-02-25 10:59:41 +0300

    Added ClientController

---
+++ web/src/main/java/by/pvt/khudnitsky/payments/controllers/ClientController.java
@@ -0,0 +1,226 @@
+package by.pvt.khudnitsky.payments.controllers;
+
+import by.pvt.khudnitsky.payments.commands.factory.CommandType;
+import by.pvt.khudnitsky.payments.entities.Account;
+import by.pvt.khudnitsky.payments.entities.User;
+import by.pvt.khudnitsky.payments.enums.AccessLevelType;
+import by.pvt.khudnitsky.payments.enums.MessageConstants;
+import by.pvt.khudnitsky.payments.enums.PagePath;
+import by.pvt.khudnitsky.payments.enums.Parameters;
+import by.pvt.khudnitsky.payments.exceptions.ServiceException;
+import by.pvt.khudnitsky.payments.managers.MessageManager;
+import by.pvt.khudnitsky.payments.managers.PagePathManager;
+import by.pvt.khudnitsky.payments.services.IAccountService;
+import by.pvt.khudnitsky.payments.services.impl.AccountServiceImpl;
+import by.pvt.khudnitsky.payments.utils.RequestParameterParser;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import javax.inject.Inject;
+import javax.jws.WebParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by: khudnitsky
+ * Date: 23.02.2016
+ * Time: 14:14
+ */
+@Component
+@RequestMapping(value = "/client")
+public class ClientController {
+    private IAccountService accountService;
+
+    private PagePathManager pagePathManager = PagePathManager.getInstance();
+    private MessageManager messageManager = MessageManager.getInstance();
+
+    //@Inject
+    public ClientController(IAccountService accountService){
+        this.accountService = accountService;
+    }
+
+    @RequestMapping(value = "/balance", method = GET)
+    public String showBalance(ModelMap model,
+                              @RequestParam(value = Parameters.USER) User user,
+                              HttpServletRequest request) {
+        String pagePath;
+        AccessLevelType accessLevelType = (AccessLevelType) model.get(Parameters.USER_ACCESS_LEVEL);
+        if(accessLevelType == AccessLevelType.CLIENT){
+            try {
+                // TODO DTO
+                Set<Account> accounts = user.getAccounts();
+                Iterator<Account> iterator = accounts.iterator();
+                Long accountId = -1L;
+                while (iterator.hasNext()){
+                    accountId = iterator.next().getId();
+                }
+                Account account = accountService.getById(accountId);
+                model.put(Parameters.OPERATION_BALANCE, account.getDeposit());
+                model.put(Parameters.ACCOUNT_CURRENCY, account.getCurrency().getCurrencyType());
+                pagePath = pagePathManager.getProperty(PagePath.CLIENT_BALANCE_PAGE_PATH);
+            }
+            catch (ServiceException e) {
+                pagePath = pagePathManager.getProperty(PagePath.ERROR_PAGE_PATH);
+                model.put(Parameters.ERROR_DATABASE, messageManager.getProperty(MessageConstants.ERROR_DATABASE));
+            }
+        }
+        else{
+            pagePath = pagePathManager.getProperty(PagePath.INDEX_PAGE_PATH);
+            request.getSession().invalidate();
+        }
+        return pagePath;
+    }
+
+    @RequestMapping(value = "/funds", method = GET)
+    public String showFundsPage(ModelMap model){
+        return pagePathManager.getProperty(PagePath.CLIENT_FUND_PAGE_PATH);
+    }
+
+    @RequestMapping(value = "funds/add", method = POST)
+    public String addFund(ModelMap model,
+                          @RequestParam(Parameters.USER_ACCESS_LEVEL) AccessLevelType accessLevelType,
+                          @RequestParam(Parameters.USER) User user,
+                          @RequestParam(Parameters.OPERATION_ADD_FUNDS) double amount,
+                          HttpServletRequest request) {
+        String pagePath;
+        if(accessLevelType == AccessLevelType.CLIENT) {
+            try {
+                // TODO DTO
+                Set<Account> accounts = user.getAccounts();
+                Iterator<Account> iterator = accounts.iterator();
+                Long accountId = -1L;
+                while (iterator.hasNext()){
+                    accountId = iterator.next().getId();
+                }
+                if (!accountService.checkAccountStatus(accountId)) {
+                    if (amount > 0) {
+                        String description = "Платеж"; // TODO вынести
+                        accountService.addFunds(user, description, amount);
+                        model.put(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.SUCCESS_OPERATION));
+                        pagePath = pagePathManager.getProperty(PagePath.CLIENT_FUND_PAGE_PATH);
+                    } else {
+                        model.put(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.NEGATIVE_ARGUMENT));
+                        pagePath = pagePathManager.getProperty(PagePath.CLIENT_FUND_PAGE_PATH);
+                    }
+                } else {
+                    pagePath = pagePathManager.getProperty(PagePath.CLIENT_BLOCK_PAGE_PATH);
+                }
+            }
+            catch (ServiceException e) {
+                model.put(Parameters.ERROR_DATABASE, messageManager.getProperty(MessageConstants.ERROR_DATABASE));
+                pagePath = pagePathManager.getProperty(PagePath.ERROR_PAGE_PATH);
+            } catch (NumberFormatException e) {
+                model.put(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.INVALID_NUMBER_FORMAT));
+                pagePath = pagePathManager.getProperty(PagePath.CLIENT_FUND_PAGE_PATH);
+            }
+        }
+        else{
+            pagePath = pagePathManager.getProperty(PagePath.INDEX_PAGE_PATH);
+            request.getSession().invalidate();
+        }
+        return pagePath;
+    }
+
+    @RequestMapping(value = "/block", method = POST)
+    public String blockAccount(ModelMap model,
+                               @RequestParam(Parameters.USER_ACCESS_LEVEL) AccessLevelType accessLevelType,
+                               @RequestParam(Parameters.USER) User user,
+                               HttpServletRequest request) {
+        String pagePath;
+        if(accessLevelType == AccessLevelType.CLIENT){
+            String description = "Блокировка счета";   // TODO
+            try {
+                // TODO DTO
+                Set<Account> accounts = user.getAccounts();
+                Iterator<Account> iterator = accounts.iterator();
+                Long accountId = -1L;
+                while (iterator.hasNext()){
+                    accountId = iterator.next().getId();
+                }
+                if(!accountService.checkAccountStatus(accountId)){
+                    accountService.blockAccount(user, description);
+                    model.put(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.SUCCESS_OPERATION));
+                    pagePath = pagePathManager.getProperty(PagePath.CLIENT_BLOCK_PAGE_PATH);
+                }
+                else{
+                    pagePath = pagePathManager.getProperty(PagePath.CLIENT_BLOCK_PAGE_PATH);
+                }
+            }
+            catch (ServiceException e) {
+                model.addAttribute(Parameters.ERROR_DATABASE, messageManager.getProperty(MessageConstants.ERROR_DATABASE));
+                pagePath = pagePathManager.getProperty(PagePath.ERROR_PAGE_PATH);
+            }
+        }
+        else{
+            pagePath = pagePathManager.getProperty(PagePath.INDEX_PAGE_PATH);
+            request.getSession().invalidate();
+        }
+        return pagePath;
+    }
+
+    @RequestMapping(value = "/payments", method = GET)
+    public String showPaymentsPage(ModelMap model){
+        return pagePathManager.getProperty(PagePath.CLIENT_PAYMENT_PAGE_PATH);
+    }
+
+    @RequestMapping(value = "/payments/payment", method = POST)
+    public String payment(ModelMap model,
+                          @RequestParam(Parameters.USER_ACCESS_LEVEL) AccessLevelType accessLevelType,
+                          @RequestParam(Parameters.USER) User user,
+                          @RequestParam(Parameters.OPERATION_PAYMENT) double amount,
+                          HttpServletRequest request) {
+        String pagePath;
+        if(accessLevelType == AccessLevelType.CLIENT){
+            try {
+                // TODO DTO
+                Set<Account> accounts = user.getAccounts();
+                Iterator<Account> iterator = accounts.iterator();
+                Long accountId = -1L;
+                while (iterator.hasNext()){
+                    accountId = iterator.next().getId();
+                }
+                if(!AccountServiceImpl.getInstance().checkAccountStatus(accountId)){
+                    if(amount > 0){
+                        Account account = accountService.getById(accountId);
+                        if(account.getDeposit() >= amount){
+                            String description = "Платеж"; // TODO
+                            accountService.payment(user, description, amount);
+                            model.addAttribute(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.SUCCESS_OPERATION));
+                            pagePath = pagePathManager.getProperty(PagePath.CLIENT_PAYMENT_PAGE_PATH);
+                        }
+                        else{
+                            model.addAttribute(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.FAILED_OPERATION));
+                            pagePath = PagePathManager.getInstance().getProperty(PagePath.CLIENT_PAYMENT_PAGE_PATH);
+                        }
+                    }
+                    else{
+                        model.addAttribute(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.NEGATIVE_ARGUMENT));
+                        pagePath = pagePathManager.getProperty(PagePath.CLIENT_PAYMENT_PAGE_PATH);
+                    }
+                }
+                else{
+                    pagePath = pagePathManager.getProperty(PagePath.CLIENT_BLOCK_PAGE_PATH);
+                }
+            }
+            catch (ServiceException e) {
+                model.addAttribute(Parameters.ERROR_DATABASE, messageManager.getProperty(MessageConstants.ERROR_DATABASE));
+                pagePath = pagePathManager.getProperty(PagePath.ERROR_PAGE_PATH);
+            }
+            catch (NumberFormatException e){
+                model.addAttribute(Parameters.OPERATION_MESSAGE, messageManager.getProperty(MessageConstants.INVALID_NUMBER_FORMAT));
+                pagePath = messageManager.getProperty(PagePath.CLIENT_PAYMENT_PAGE_PATH);
+            }
+        }
+        else{
+            pagePath = pagePathManager.getProperty(PagePath.INDEX_PAGE_PATH);
+            request.getSession().invalidate();
+        }
+        return pagePath;
+    }
+}
