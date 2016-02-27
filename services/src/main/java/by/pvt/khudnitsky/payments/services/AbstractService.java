b4498265d5ef9bedc70bf8da42f757a4da864cc2
khudnitsky <khudnitsky@gmail.com>
2016-02-28 02:52:27 +0300

    Added program transactions

--- services/src/main/java/by/pvt/khudnitsky/payments/services/AbstractService.java
+++ services/src/main/java/by/pvt/khudnitsky/payments/services/AbstractService.java
@@ -2,17 +2,16 @@ package by.pvt.khudnitsky.payments.services;
 
 import by.pvt.khudnitsky.payments.dao.IDao;
 import by.pvt.khudnitsky.payments.entities.AbstractEntity;
-import by.pvt.khudnitsky.payments.entities.Account;
 import by.pvt.khudnitsky.payments.exceptions.DaoException;
 import by.pvt.khudnitsky.payments.exceptions.ServiceException;
-import by.pvt.khudnitsky.payments.utils.HibernateUtil;
-import by.pvt.khudnitsky.payments.utils.TransactionUtil;
 import org.apache.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.io.Serializable;
-import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -20,11 +19,12 @@ import java.util.List;
  */
 public abstract class AbstractService<T extends AbstractEntity> implements IService<T>{
     private static Logger logger = Logger.getLogger(AbstractService.class);
-    private IDao dao;
-
-    protected static HibernateUtil util = HibernateUtil.getInstance();
     protected final String TRANSACTION_SUCCEEDED = "Transaction succeeded";
     protected final String TRANSACTION_FAILED = "Error was thrown in service: ";
+    private IDao dao;
+
+    @Autowired
+    protected TransactionTemplate transactionTemplate;
 
     protected AbstractService(IDao dao){
         this.dao = dao;
@@ -32,96 +32,104 @@ public abstract class AbstractService<T extends AbstractEntity> implements IServ
 
     @Override
     public void delete(Long id) throws ServiceException {
-        Session session = util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            dao.delete(id);
-            transaction.commit();
-            logger.info(TRANSACTION_SUCCEEDED);
-            logger.info("Deleted entity #" + id);
-        }
-        catch (DaoException e) {
-            TransactionUtil.rollback(transaction, e);
-            logger.error(TRANSACTION_FAILED, e);
-            throw new ServiceException(TRANSACTION_FAILED + e);
-        }
+        transactionTemplate.execute(new TransactionCallback<Void>() {
+            @Override
+            public Void doInTransaction(TransactionStatus transactionStatus){
+                try {
+                    dao.delete(id);
+                    logger.info(TRANSACTION_SUCCEEDED);
+                    logger.info("Deleted entity #" + id);
+                }
+                catch (DaoException e) {
+                    logger.error(TRANSACTION_FAILED, e);
+                    transactionStatus.setRollbackOnly();
+                    //throw new ServiceException(TRANSACTION_FAILED + e);
+                }
+                return null;
+            }
+        });
     }
+
 
     @Override
     public List<T> getAll() throws ServiceException {
-        List<T> list;
-        Session session = util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            list = dao.getAll();
-            transaction.commit();
-            logger.info(TRANSACTION_SUCCEEDED);
-            logger.info(list);
-        }
-        catch (DaoException e) {
-            TransactionUtil.rollback(transaction, e);
-            logger.error(TRANSACTION_FAILED, e);
-            throw new ServiceException(TRANSACTION_FAILED + e);
-        }
-        return list;
+        return transactionTemplate.execute(new TransactionCallback<List<T>>() {
+            @Override
+            public List<T> doInTransaction(TransactionStatus transactionStatus) {
+                List<T> list = new ArrayList<T>();
+                try {
+                    list = dao.getAll();
+                    logger.info(TRANSACTION_SUCCEEDED);
+                    logger.info(list);
+                }
+                catch (DaoException e) {
+                    logger.error(TRANSACTION_FAILED, e);
+                    transactionStatus.setRollbackOnly();
+                    //throw new ServiceException(TRANSACTION_FAILED + e);
+                }
+                return list;
+            }
+        });
     }
 
     @Override
     public T getById(Long id) throws ServiceException {
-        T entity;
-        Session session = util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            entity = (T) dao.getById(id);
-            transaction.commit();
-            logger.info(entity);
-        }
-        catch (DaoException e) {
-            TransactionUtil.rollback(transaction, e);
-            logger.error(TRANSACTION_FAILED, e);
-            throw new ServiceException(TRANSACTION_FAILED + e);
-        }
-        return entity;
+        return transactionTemplate.execute(new TransactionCallback<T>() {
+            @Override
+            public T doInTransaction(TransactionStatus transactionStatus) {
+                T entity = null;
+                try {
+                    entity = (T) dao.getById(id);
+                    logger.info(entity);
+                }
+                catch (DaoException e) {
+                    logger.error(TRANSACTION_FAILED, e);
+                    transactionStatus.setRollbackOnly();
+                    //throw new ServiceException(TRANSACTION_FAILED + e);
+                }
+                return entity;
+            }
+        });
+
     }
 
     @Override
     public Serializable save(T entity) throws ServiceException {
-        Serializable id;
-        Session session = util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            id = dao.save(entity);
-            transaction.commit();
-            logger.info(TRANSACTION_SUCCEEDED);
-            logger.info(entity);
-        }
-        catch (DaoException e) {
-            TransactionUtil.rollback(transaction, e);
-            logger.error(TRANSACTION_FAILED, e);
-            throw new ServiceException(TRANSACTION_FAILED + e);
-        }
-        return id;
+        return transactionTemplate.execute(new TransactionCallback<Serializable>() {
+            @Override
+            public Serializable doInTransaction(TransactionStatus transactionStatus) {
+                Serializable id = null;
+                try {
+                    id = dao.save(entity);
+                    logger.info(TRANSACTION_SUCCEEDED);
+                    logger.info(entity);
+                }
+                catch (DaoException e) {
+                    logger.error(TRANSACTION_FAILED, e);
+                    transactionStatus.setRollbackOnly();
+                    //throw new ServiceException(TRANSACTION_FAILED + e);
+                }
+                return id;
+            }
+        });
     }
 
     @Override
     public void update(T entity) throws ServiceException {
-        Session session = util.getSession();
-        Transaction transaction = null;
-        try {
-            transaction = session.beginTransaction();
-            dao.update(entity);
-            transaction.commit();
-            logger.info(TRANSACTION_SUCCEEDED);
-            logger.info(entity);
-        }
-        catch (DaoException e) {
-            TransactionUtil.rollback(transaction, e);
-            logger.error(TRANSACTION_FAILED, e);
-            throw new ServiceException(TRANSACTION_FAILED + e);
-        }
+        transactionTemplate.execute(new TransactionCallback<Void>() {
+            @Override
+            public Void doInTransaction(TransactionStatus transactionStatus) {
+                try {
+                    dao.update(entity);
+                    logger.info(TRANSACTION_SUCCEEDED);
+                    logger.info(entity);
+                } catch (DaoException e) {
+                    logger.error(TRANSACTION_FAILED, e);
+                    transactionStatus.setRollbackOnly();
+                    //throw new ServiceException(TRANSACTION_FAILED + e);
+                }
+                return null;
+            }
+        });
     }
 }
