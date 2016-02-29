bc61de4f79ae1d0a6bd41d67dcd2d45bf00b2964
khudnitsky <khudnitsky@gmail.com>
2016-02-29 20:59:06 +0300

    Added localization to controllers

---
+++ web/src/main/java/by/pvt/khudnitsky/payments/authentication/AuthenticationService.java
@@ -0,0 +1,57 @@
+package by.pvt.khudnitsky.payments.authentication;
+
+import by.pvt.khudnitsky.payments.entities.AccessLevel;
+import by.pvt.khudnitsky.payments.entities.User;
+import by.pvt.khudnitsky.payments.exceptions.ServiceException;
+import by.pvt.khudnitsky.payments.services.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by: khudnitsky
+ * Date: 29.02.2016
+ * Time: 15:18
+ */
+
+
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        User user = null;
+        try {
+            user = userService.getUserByLogin(login);
+            if (user == null) {
+                throw new UsernameNotFoundException("Username not found");
+            }
+        }
+        catch (ServiceException e) {
+            e.printStackTrace();
+        }
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPassword(),
+                true, true, true, true, getGrantedAuthorities(user));
+    }
+
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        for (AccessLevel access : user.getAccessLevels()) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + access.getAccessLevelType().toString()));
+        }
+        return authorities;
+    }
+}
